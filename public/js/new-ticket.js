08038b88d0b80cfbeb788734dd9a4ed388ae2a18
Luis Angel <angelfgdeveloper@gmail.com>
2024-08-31 20:20:38 -0600

    Crear un ticket en front

--- public/js/new-ticket.js
+++ public/js/new-ticket.js
@@ -1,3 +1,24 @@
 
+// console.log('Nuevo Ticket HTML');
 
-console.log('Nuevo Ticket HTML');
+const currentTicketbl = document.querySelector('span');
+const createTicketBtn = document.querySelector('button');
+
+
+async function getLastTicket() {
+  const lastTicket = await fetch('/api/ticket/last').then(resp => resp.json());
+  currentTicketbl.innerText = lastTicket;
+  // console.log(lastTicket);
+}
+
+async function createTicket() {
+  const newTicket = await fetch('/api/ticket', {
+    method: 'POST'
+  }).then(resp => resp.json());
+
+  currentTicketbl.innerText = newTicket.number;
+}
+
+createTicketBtn.addEventListener('click', createTicket);
+
+getLastTicket();
